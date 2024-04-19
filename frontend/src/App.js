6409fedbe1d831e62acc5fa791ddfded988828f2
avinashsah0811 <avinashsah0811@gmail.com>
2024-04-19 17:49:12 +0530

    Revert "server folder created, user data schema created, data storing in database, registration is working from frontend and login is working in frontend with data checking from the database"
    This reverts commit c665842ed241cb370c1095a9a0855c2a81c05457.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import LoginPage from './components/LoginPage';
 import SignUpPage from './components/SignUpPage';
-import HomePage from './components/HomePage';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/home" element={<HomePage />} />
       </Routes>
     </>
   );
